95e39b64417664802b399b935e0d92c11d54e016
Christian Wells <christian@profitwell.com>
2019-07-01 13:07:39 -0400

    add identity back, fill out tests

--- test/index.test.js
+++ test/index.test.js
@@ -11,6 +11,7 @@ describe('Profitwell', function() {
   var options = {
     publicApiToken: '123123123'
   };
+  var testEmail = 'testEmail@pw.com';
 
   beforeEach(function() {
     analytics = new Analytics();
@@ -31,41 +32,85 @@ describe('Profitwell', function() {
   });
 
   describe('before loading', function() {
-    beforeEach(function() {
-      analytics.stub(profitwell, 'load');
-    });
-
-    afterEach(function() {
-      profitwell.reset();
-    });
-
     describe('#initialize', function() {
+      beforeEach(function() {
+        analytics.stub(profitwell, 'load');
+        analytics.stub(profitwell, 'start');
+      });
+
       it('should call load on initialize', function() {
         analytics.initialize();
+
         analytics.called(profitwell.load);
       });
 
       it('should create profitwell object', function() {
         analytics.initialize();
+
         analytics.assert(window.profitwell instanceof Function);
       });
-      it('should call start on initialize', function() {
-        analytics.stub(window, 'profitwell');
-        analytics.identify('123', {
-          email: 'mycoolemail@pw.com'
-        });
+
+      it('should call start with email on initialize', function() {
+        addTraitsToStorage()
         analytics.initialize();
-        analytics.called(window.profitwell, 'start', {
-          user_email: 'mycoolemail@pw.com'
-        });
+
+        analytics.called(profitwell.start, testEmail);
       });
 
-      it('should call empty start on initialize', function() {
-        analytics.stub(window, 'profitwell');
-        analytics.spy(window.profitwell);
+      it('should call start with nothing on anonymous initialize', function() {
+        profitwell.options.anonymousOnly = true;
         analytics.initialize();
+
+        analytics.called(profitwell.start);
+      })
+    });
+
+    describe('#start', function() {
+      beforeEach(function() {
+        analytics.stub(window, 'profitwell');
+      });
+
+      it('should call start with user\'s email', function() {
+        profitwell.start(testEmail);
+
+        analytics.called(window.profitwell, 'start', { user_email: testEmail });
+      });
+
+      it('should call start with an empty object', function() {
+        profitwell.start();
+
         analytics.called(window.profitwell, 'start', {});
       });
     });
   });
+
+  describe('after loading', function() {
+    beforeEach(function(done) {
+      analytics.once('ready', function() {
+        analytics.stub(profitwell, 'start');
+        done();
+      });
+      analytics.initialize();
+    });
+
+    describe('#identify', function() {
+      it('should call start with the user\'s email', function() {
+        analytics.identify(testEmail);
+        analytics.called(profitwell.start, testEmail);
+      });
+
+      it('should call start with an empty object', function() {
+        analytics.identify();
+        analytics.called(profitwell.start);
+      });
+    });
+  });
+
+  function addTraitsToStorage() {
+    // this is a hack to get analytics to initialize a user with an email
+    localStorage.setItem(
+      'ajs_user_traits', 
+      JSON.stringify({ email: testEmail })
+    );
+  }
 });
